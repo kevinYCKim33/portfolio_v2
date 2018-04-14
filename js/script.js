e4eefaee44d8a4cb0985b6a8a7e0faa31bcdd202
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-04-13 18:20:36 -0700

    added Typed.js

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
     play: 5000,
     pagination: false
   });
+
+// https://github.com/mattboldt/typed.js/
+  let typed = new Typed(".typed", {
+    strings: ["Software Engineer.", "Full Stack Developer.", "Tech Blogger."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
 });
