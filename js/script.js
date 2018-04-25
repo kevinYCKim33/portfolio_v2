8e019c0783b0753f2891366f047ac295d3038e46
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-04-24 23:28:20 -0700

    title slide much less stocky

--- js/script.js
+++ js/script.js
@@ -17,19 +17,19 @@ $(window).on("load", function() {
 
 $(document).ready(function(){
   $('#slides').superslides({
-    animation: 'fade',
-    play: 5000,
-    pagination: false
+    // animation: 'fade',
+    // play: 5000,
+    // pagination: false
   });
 
 // https://github.com/mattboldt/typed.js/
-  let typed = new Typed(".typed", {
-    strings: ["Software Engineer.", "Full Stack Developer.", "Tech Blogger."],
-    typeSpeed: 70,
-    loop: true,
-    startDelay: 1000,
-    showCursor: false
-  });
+  // let typed = new Typed(".typed", {
+  //   strings: ["Software Engineer.", "Full Stack Developer.", "Tech Blogger."],
+  //   typeSpeed: 70,
+  //   loop: true,
+  //   startDelay: 1000,
+  //   showCursor: false
+  // });
 
 // https://owlcarousel2.github.io/OwlCarousel2/demos/basic.html
 // if width is 480px, then show 2 items
