a43a5a77822d3a4460e2795b288f2d39b75755ed
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-04-26 00:21:39 -0700

    porto looks presentable enough

--- js/script.js
+++ js/script.js
@@ -77,7 +77,7 @@ $(document).ready(function(){
 
 
   var skillsTopOffset = $(".skillsSection").offset().top;
-  var statsTopOffset = $(".statsSection").offset().top;
+  // var statsTopOffset = $(".statsSection").offset().top;
   var countUpFinished = false;
 
   $(window).scroll(function() {
@@ -97,16 +97,16 @@ $(document).ready(function(){
       });
     }
 
-    if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-      $(".counter").each(function(){
-        let element= $(this);
-        let endVal = parseInt(element.text());
-
-        element.countup(endVal);
-      });
-      // scrolling magic to prevent the numbers from going down to 0 again.
-      countUpFinished = true;
-    }
+    // if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+    //   $(".counter").each(function(){
+    //     let element= $(this);
+    //     let endVal = parseInt(element.text());
+    //
+    //     element.countup(endVal);
+    //   });
+    //   // scrolling magic to prevent the numbers from going down to 0 again.
+    //   countUpFinished = true;
+    // }
 
 
 
