3620cb88ee092439f42d8c58d6fc9318f1ed88ce
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-04-28 00:15:44 -0700

    exp with scroll locks

--- js/script.js
+++ js/script.js
@@ -146,6 +146,8 @@ $(document).ready(function(){
   // code that scrolls down to the clicked link instead of jumping to it
   $("#navigation li a").click(function(e) {
     e.preventDefault();
+    // $(".nav-link").css("color", "white"); //doesn't work on section changes via scroll
+    // $(this).css("color","yellow");
     var targetElement = $(this).attr("href");
     var targetPosition = $(targetElement).offset().top;
     $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
@@ -153,7 +155,7 @@ $(document).ready(function(){
 
   const nav = $("#navigation");
   const navTop = nav.offset().top; //moment at which to add or remove the sticky class;
-
+  // const portfolioTop = $("#portfolio").offset().top
   $(window).on("scroll", stickyNavigation);
 
   function stickyNavigation() {
@@ -166,8 +168,12 @@ $(document).ready(function(){
     else {
       body.css("padding-top", 0);
       body.removeClass("fixedNav");
-
     }
+
+    // fiddle around with this after everything is done
+    // if ($(window).scrollTop() + 60 >= portfolioTop) {
+    //   $(".nav-link").css("color", "green"); //doesn't work on section changes via scroll
+    // }
   }
 
 });
